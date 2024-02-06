056f30dacb8bde2ecd69e95beeb1a3f5b243a2df
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-02-06 17:08:44 -0400

    structure of files and welcome page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Bienvenido, Guajon.</h1>
+
+    <iframe width="560" 
+    height="315"
+    src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=vWZGSEBX_qQSUZNu"
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    allowfullscreen>
+
+    </iframe>
     </div>
   );
 }
