fe2ecc37dc647b93cd0104fa8574d06e8e1fbb02
Edwin Camuy <edwin.camuy@upr.edu>
2024-02-22 08:16:34 -0400

    Abstracted get_all method into base DAO superclass.

--- Backend/src/dao.py
+++ Backend/src/dao.py
@@ -1,13 +1,34 @@
 import psycopg2
+from psycopg2 import sql
 from psycopg2.extensions import connection
 from dataclasses import dataclass
-from enum import Enum 
+from enum import Enum
 from fastapi import HTTPException
 
 node_type = str
 
+
+class DAO:
+    @classmethod
+    def get_all(cls, table: str, db: connection) -> list:
+        """Get all entities in a list."""
+        with db.cursor() as curs:
+            try:
+                curs.execute(
+                    sql.SQL("""
+                    SELECT * FROM {}
+                    """).format(sql.Identifier(table))
+                )
+            except psycopg2.Error as e:
+                print("Error executing SQL query:", e)
+                raise HTTPException(status_code=500, detail="Database error")
+
+            # Unpack the tuples into constructor
+            return [cls(*row) for row in curs.fetchall()]
+
+
 @dataclass
-class Node:
+class Node(DAO):
     """Node DAO"""
     nid: int
     ntype: node_type
@@ -15,18 +36,6 @@ class Node:
     nlongitude: float
     ndescription: str
 
-    @staticmethod
-    def get_all(db: connection):
-        """Get all nodes in a list."""
-        with db.cursor() as curs:
-            try:
-                curs.execute(
-                    """
-                    SELECT * FROM node
-                    """
-                )
-            except psycopg2.Error as e:
-                print("Error executing SQL query:", e)
-                raise HTTPException(status_code=500, detail="Database error")
-
-            return [Node(*row) for row in curs.fetchall()]  # Unpack the tuples into constructor
+    @classmethod
+    def get_all(cls, db: connection):
+        return super().get_all('node', db)
