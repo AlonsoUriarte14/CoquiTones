5cac7e054489fa9f8def7916a12e5c1c36cd6c57
Edwin Camuy <edwin.camuy@upr.edu>
2024-02-22 11:56:12 -0400

    Timestamp index DAO

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -8,11 +8,18 @@ app = FastAPI()
 
 app.mount("/static", staticfiles.StaticFiles(directory="../../Frontend/build/static"), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def root():
     with open("../../Frontend/build/index.html", "r") as f:
         return f.read()
-    
+
+
 @app.get("/api/node/all")
-async def node(db = Depends(get_db_connection)):
+async def node_all(db=Depends(get_db_connection)):
     return dao.Node.get_all(db)
+
+
+@app.get("/api/timestamp/all")
+async def timestamp_all(db=Depends(get_db_connection)):
+    return dao.TimestampIndex.get_all(db)
--- Backend/src/dao.py
+++ Backend/src/dao.py
@@ -2,7 +2,7 @@ import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import connection
 from dataclasses import dataclass
-from enum import Enum
+from datetime import datetime
 from fastapi import HTTPException
 
 node_type = str
@@ -10,14 +10,14 @@ node_type = str
 
 class DAO:
     @classmethod
-    def get_all(cls, table: str, db: connection) -> list:
+    def get_all(cls, db: connection) -> list:
         """Get all entities in a list."""
         with db.cursor() as curs:
             try:
                 curs.execute(
                     sql.SQL("""
                     SELECT * FROM {}
-                    """).format(sql.Identifier(table))
+                    """).format(sql.Identifier(cls.table))
                 )
             except psycopg2.Error as e:
                 print("Error executing SQL query:", e)
@@ -36,6 +36,14 @@ class Node(DAO):
     nlongitude: float
     ndescription: str
 
-    @classmethod
-    def get_all(cls, db: connection):
-        return super().get_all('node', db)
+    table = "node"
+
+
+@dataclass
+class TimestampIndex(DAO):
+    """Timestamp index DAO"""
+    tid: int
+    nid: int
+    ttime: datetime
+
+    table = "timestampindex"
