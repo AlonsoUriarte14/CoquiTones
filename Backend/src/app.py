2ac242717fafbd060de173327f52205a71328943
Edwin Camuy <90163465+EdwinC1339@users.noreply.github.com>
2024-02-23 01:57:21 -0400

    DAO
    * Timestamp index DAO
    
    * Classifier report DAO
    
    * Weather data DAO
    
    * Get one and audio file
    
    * Added get element by ID endpoints to API
    
    * Audio API endpoints

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, staticfiles, Depends, HTTPException
-from fastapi.responses import HTMLResponse
+
+from fastapi.responses import HTMLResponse, Response
 from dbutil import get_db_connection
 import psycopg2
 import dao
@@ -8,11 +9,64 @@ app = FastAPI()
 
 app.mount("/static", staticfiles.StaticFiles(directory="../../Frontend/build/static"), name="static")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def root():
     with open("../../Frontend/build/index.html", "r") as f:
         return f.read()
-    
+
+
 @app.get("/api/node/all")
-async def node(db = Depends(get_db_connection)):
+async def node_all(db=Depends(get_db_connection)):
     return dao.Node.get_all(db)
+
+
+@app.get("/api/node/{nid}")
+async def node_get(nid: int, db=Depends(get_db_connection)):
+    return dao.Node.get(nid, db)
+
+
+@app.get("/api/timestamp/all")
+async def timestamp_all(db=Depends(get_db_connection)):
+    return dao.TimestampIndex.get_all(db)
+
+
+@app.get("/api/timestamp/{tid}")
+async def timestamp_get(tid: int, db=Depends(get_db_connection)):
+    return dao.TimestampIndex.get(tid, db)
+
+
+@app.get("/api/report/all")
+async def report_all(db=Depends(get_db_connection)):
+    return dao.ClassifierReport.get_all(db)
+
+
+@app.get("/api/report/{crid}")
+async def report_get(crid: int, db=Depends(get_db_connection)):
+    return dao.ClassifierReport.get(crid, db)
+
+
+@app.get("/api/weather/all")
+async def weather_all(db=Depends(get_db_connection)):
+    return dao.WeatherData.get_all(db)
+
+
+@app.get("/api/report/{wdid}")
+async def report_get(wdid: int, db=Depends(get_db_connection)):
+    return dao.WeatherData.get(wdid, db)
+
+
+@app.get("/api/audio/all")
+async def audio_all(db=Depends(get_db_connection)):
+    return dao.AudioFile.get_all(db)
+
+
+@app.get(
+    path="/api/audio/{afid}",
+    response_class=Response
+)
+async def audio_get(afid: int, db=Depends(get_db_connection)):
+    audio_file = dao.AudioFile.get(afid, db)
+    data = audio_file.data
+
+    return Response(content=data, media_type="audio/mpeg")
