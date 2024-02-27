68d8e70efdeb7ab58c24978fa1eaa8d29a3154c5
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-02-27 11:07:05 -0400

    refactored a lil

--- Frontend/Util/DataHandler.js
+++ Frontend/Util/DataHandler.js
@@ -2,85 +2,45 @@
 import axios from 'axios';
 import websiteUrl from './WebUrlConfig';
 
-// @app.get("/", response_class=HTMLResponse)
-// async def root():
-//     with open("../../Frontend/build/index.html", "r") as f:
-//         return f.read()
 
-
-
+class ValidationError extends Error {
+    constructor(message) {
+        super(message); // (1)
+        this.name = "ValidationError"; // (2)
+    }
+}
 export default class DataHandler {
 
-    constructor(){}
+    allowedEndpointTypes = ["node", "timestamp", "report", "weather", "audio",]
+    constructor(endpointType){
+        
+        if (this.allowedEndpointTypes.includes(endpointType)){
+            this.endopointType = endpointType;
+        }
+        
+        else{
+            throw new ValidationError(`Endpoint "${endpointType}" is not one of the defined endpoints`)
+        }
+    }
+    
 
-
-    get_all_nodes = () => {
-        axios.get(websiteUrl + '/api/node/all')
+    get_all() {
+        axios.get(`${websiteUrl}/api/${this.endpointType}/all`)
         .then( res => {
             return res.data
         })
     }
 
-    get_node_from_id = (nid) => {
-        axios.get(websiteUrl + '/api/node/' + nid)
+    get_from_id = (id) => {
+        axios.get(`${websiteUrl}/api/${this.endpointType}/${id}`)
         .then( res => {
             return res.data
         })
     }
 
-    get_timestamp_all = () => {
-        axios.get(websiteUrl + '/api/timestamp/all')
+    get_from_timestamp = (timestamp) => {
+        axios.get(`${websiteUrl}/api/${this.endpointType}/timestamp?${timestamp}`)
         .then(res => {
-            return res.data
-        })
-    }
-
-    get_timestamp_from_id = (tid) => {
-        axios.get(websiteUrl + '/api/timestamp/'  + tid)
-        .then(res => {
-            return res.data
-        })
-    }
-
-
-    get_all_reports = () => {
-        axios.get(websiteUrl + '/api/report/all')
-        .then(res => {
-            return res.data
-        })
-    }
-
-    get_report_from_id = (crid)  => {
-        axios.get(websiteUrl + '/api/report/' + crid)
-        .then(res => {
-            return res.data
-        })
-    }
-
-    get_all_weather = () => {
-        axios.get(websiteUrl + '/api/weather/all')
-        .then(res => {
-            return res.data
-        })
-    }
-
-    get_weather_from_id = (wdid) => {
-        axios.get(websiteUrl + '/api/weather/'  + wdid)
-        .then(res => {
-            return res.data
-        })
-    }
-
-    get_all_audio = () => {
-        axios.get(websiteUrl + 'api/audio/all')
-        .then(res => {
-            return res.data
-        })
-    }
-
-    get_audio_from_id = (afid) => {
-        axios.get(websiteUrl + '/api/audio/' + afid)
-        .then( res => {
             return res.data
         })
     }
