360cd1b11171e23865073721b752e672adf14dad
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-03-01 11:17:16 -0400

    Abstracted nav bar and App Bar to use in all pages

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 
@@ -14,16 +14,13 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-
-import { mainListItems, secondaryListItems } from '../components/dashboard/listItems';
 import Chart from '../components/dashboard/Chart';
 import Deposits from '../components/dashboard/Deposits';
 import Orders from '../components/dashboard/Orders';
 import AppBar from '../components/shared/AppBar'
 import Drawer from '../components/shared/Drawer';
+
+import BarAndNav from '../components/shared/AppBar';
 
 function Copyright(props) {
 	return (
@@ -46,81 +43,24 @@ function Copyright(props) {
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme({
-  palette: {
-    primary: {
-		main:  "#FE855D"
-    },
-    secondary: {
-		main: "#FE855D"
-    },
-  },
+	palette: {
+		primary: {
+			main: "#FE855D"
+		},
+		secondary: {
+			main: "#FE855D"
+		},
+	},
 });
 
 export default function Dashboard() {
-	const [open, setOpen] = React.useState(true);
-	const toggleDrawer = () => {
-		setOpen(!open);
-	};
+
 
 	return (
 		<ThemeProvider theme={defaultTheme}>
 			<Box sx={{ display: 'flex' }}>
 				<CssBaseline />
-				<AppBar position="absolute" open={open}>
-					<Toolbar
-						sx={{
-							pr: '24px', // keep right padding when drawer closed
-						}}
-					>
-						<IconButton
-							edge="start"
-							color="inherit"
-							aria-label="open drawer"
-							onClick={toggleDrawer}
-							sx={{
-								marginRight: '36px',
-								...(open && { display: 'none' }),
-							}}
-						>
-							<MenuIcon />
-						</IconButton>
-						<Typography
-							component="h1"
-							align="center"
-							variant="h4"
-							color="inherit"
-							noWrap
-							sx={{ flexGrow: 1 }}
-							
-						>
-							CoquiTones Dashboard
-						</Typography>
-					</Toolbar>
-				</AppBar>
-				<Drawer variant="permanent" open={open}>
-					<Toolbar
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
-							justifyContent: 'flex-end',
-							px: [1],
-						}}
-					>
-						<Typography component="h1" variant = 'h5' align='center' color = 'primary'sx={{ flexGrow: 1 }}> 
-							Applications 
-						</Typography>
-						
-						<IconButton onClick={toggleDrawer}>
-							<ChevronLeftIcon />
-						</IconButton>
-					</Toolbar>
-					<Divider />
-					<List component="nav">
-						{mainListItems}
-						<Divider sx={{ my: 1 }} />
-						{secondaryListItems}
-					</List>
-				</Drawer>
+				<BarAndNav />
 				<Box
 					component="main"
 					sx={{
