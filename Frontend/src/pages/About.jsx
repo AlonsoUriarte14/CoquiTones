dd1eb92404f209e2a54beec853508bb18cff96d2
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-03-30 16:07:29 -0400

    added about pretty and about

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -2,146 +2,119 @@ import React from "react";
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 import DiegoPic from '../components/images/DiegoAlonsoPFP.jpeg'
 import EdwinPic from '../components/images/EdwinCamuyPFP.png'
 import RolandoPic from '../components/images/RolandoRiosPFP.jpeg'
-import BarAndNav from "../components/shared/BarAndNav";
-import theme from "../components/shared/Theme"
-import FileUpload from "../components/shared/FileUpload";
 import Navbar from "../components/shared/Navbar";
+import theme from "../components/shared/Theme";
 
-const studentList = [{ name: "Diego A. Santiago Uriarte", image: DiegoPic, bio: "Software Engineer Student" },
-{ name: "Edwin J. Camuy Medina", image: EdwinPic, bio: 'Software Engineer Student' },
-{ name: "Rolando Ríos Bonilla", image: RolandoPic, bio: 'Software Engineer Student' }];
+const studentList = [
+    { name: "Diego A. Santiago Uriarte", image: DiegoPic, bio: "Software Engineer Student" },
+    { name: "Edwin J. Camuy Medina", image: EdwinPic, bio: 'Software Engineer Student' },
+    { name: "Rolando Ríos Bonilla", image: RolandoPic, bio: 'Software Engineer Student' }
+];
 
 const About = () => {
 
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             <Navbar />
-            <Box sx={{ display: 'flex' }} >
-                <CssBaseline />
+            <Container maxWidth="lg" sx={{ mt: 10, mb: 10 }}>
+                <Grid container spacing={4}>
+                    <Grid item xs={12} md={8} lg={9}>
+                        <Paper sx={{ p: 4 }}>
+                            <Paper >
 
-                <Box
-                    component="main"
-                    sx={{
-                        backgroundColor: (theme) =>
-                            theme.palette.mode === 'light'
-                                ? theme.palette.grey[100]
-                                : theme.palette.grey[900],
-                        flexGrow: 1,
-                        height: '100vh',
-                        overflow: 'auto',
-                    }}
-                >
-                    <Container maxWidth="lg" sx={{ mt: 10, mb: 10 }}>
-                        <Grid item xs={12} md={8} lg={9}>
-                            <Paper
-                                sx={{
-                                    p: 2,
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    height: "auto",
-                                }}
-                            >
-                                <Typography variant="h1" color="primary" align="center">
-                                    About Page
+                                <Typography variant="h2" color="primary" align="center" gutterBottom>
+                                    About
                                 </Typography>
+                            </Paper>
 
-                                <hr />
+                            <Typography variant="h4" color="white" align="center" gutterBottom>
+                                Mission Statement
+                            </Typography>
 
-                                <Typography variant="h5" color="white" align="center">
-                                    Mission Statement
-                                </Typography >
-                                <Typography variant="p" color="white" align="left">
-
-                                    One of the many beautiful things in Puerto Rico is its nature,
-                                    with the coquí frog at the forefront as a cultural symbol of Puerto Rico.
-                                    Unfortunately, due to widespread issues like deforestation
-                                    and forest fires, around 60% of coquís are in danger of going extinct.
-                                    CoquiTones, in collaboration with Proyecto Coqui, are
-                                    committed to the conservation of the coqui species.
-                                    Our mission is to enhance hardware implementation,
-                                    streamline software usability, and optimize data
-                                    structure to bolster the technological aspects of preservation.
+                            <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                <Typography variant="body1" color="white" align="left">
+                                    One of the many beautiful things in Puerto Rico is its nature, with the coquí frog at the forefront as a cultural symbol of Puerto Rico. Unfortunately, due to widespread issues like deforestation and forest fires, around 60% of coquís are in danger of going extinct. CoquiTones, in collaboration with Proyecto Coqui, are committed to the conservation of the coqui species. Our mission is to enhance hardware implementation, streamline software usability, and optimize data structure to bolster the technological aspects of preservation.
                                 </Typography>
+                            </Paper>
 
-                                <Typography variant='h5' color="white" align="center">
+                            <Typography variant="h4" color="white" align="center" sx={{ mt: 4 }} gutterBottom>
+                                Who We Are
+                            </Typography>
 
-                                    Who We Are
+                            <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                <Typography variant="body1" color="white">
+                                    We are a trio of students from the University of Puerto Rico Mayaguez engaged in our Capstone Project. Drawing upon our collective academic journey, we are leveraging our knowledge and acquiring new skills essential for the project's success.
                                 </Typography>
+                            </Paper>
 
-                                <Typography variant='p' color="white">
+                            <Grid container spacing={4} sx={{ mt: 4 }}>
+                                {studentList.map((item, index) => (
+                                    <Grid item key={index} xs={12} md={4}>
+                                        <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                            <img src={item.image} alt={item.name} style={{ width: '100%', borderRadius: '50%' }} />
+                                            <Typography variant="subtitle1" color="white" align="center" sx={{ mt: 2 }}>
+                                                {item.name}
+                                            </Typography>
+                                            <Typography variant="body2" color="white" align="center">
+                                                {item.bio}
+                                            </Typography>
+                                        </Paper>
+                                    </Grid>
+                                ))}
+                            </Grid>
 
-                                    We are a trio of students from the University of Puerto Rico Mayaguez engaged in our Capstone Project.
-                                    Drawing upon our collective academic journey, we are leveraging our knowledge and acquiring new skills essential for the project's success.
-                                    [MORE NEEDED]
-                                </Typography>
-                                <Box sx={{
-                                    pt: 4,
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                    gap: 4, width: '100'
-                                }}>
+                            <Typography variant="h4" color="white" align="center" sx={{ mt: 4 }} gutterBottom>
+                                How we Collaborated
+                            </Typography>
 
-                                    {studentList.map((item, index) => (
-                                        <div key={index} style={{ textAlign: 'center' }}>
-                                            <img src={item.image} alt={item.name} style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
-                                            <Typography>{item.name}</Typography>
-                                            <Typography gutterBottom='true'>{item.bio}</Typography>
-                                        </div>
-                                    ))}
-
-                                </Box>
-
-                                <Typography variant="h5" color="white" align="center">
-
-                                    How we Collaborated
-                                </Typography>
-
-                                <Typography variant="p" color="white">
-
+                            <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                <Typography variant="body1" color="white">
                                     Facing various challenges such as manual audio analysis of large volumes of data, ongoing device maintenance, and inconsistent data storage, Proyecto Coqui sought solutions. CoquiTones brought it to them through the implementation of technological advances, bringing efficiency across all stages of the preservation processes undertaken by Proyecto Coqui.
                                 </Typography>
+                            </Paper>
 
-                                <Typography variant="h5" color="white" align="center">
+                            <Typography variant="h4" color="white" align="center" sx={{ mt: 4 }} gutterBottom>
+                                Hardware
+                            </Typography>
 
-
-                                    Hardware
-                                </Typography>
-                                <Typography variant="p" color="white" >
-
-
+                            <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                <Typography variant="body1" color="white">
                                     By integrating an IoT network, we have created interconnected nodes equipped with multiple sensors spanning a wider area within the forest. These nodes capture both acoustic and meteorological data at 5-minute intervals within a 30-minute cycle. All collected data is transmitted via LTE communications to a remote server. To minimize device retrieval frequency, we've engineered a robust power supply system with the ability to harness solar energy, extending operational lifespan to a minimum of three weeks.
                                 </Typography>
+                            </Paper>
 
+                            <Typography variant="h4" color="white" align="center" sx={{ mt: 4 }} gutterBottom>
+                                Software
+                            </Typography>
 
-                                <Typography variant="h5" color="white" align="center">
-
-
-                                    Software
-                                </Typography>
-                                <Typography variant="p" color="white">
-
-
+                            <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                <Typography variant="body1" color="white">
                                     We structured the collected data utilizing a Database Management System (DBMS) to provide efficient data manipulation capabilities as required, increasing productivity and facilitating the process. We will also employ a Sound Analysis Tool to transform audio files into spectrograms, further enhancing efficiency. Additionally, we are in the process of developing a machine learning model tasked with automating the identification of the coqui species inside said audio files, which will significantly streamline the workflow and improve productivity.
-                                    Future of CoquiTones
+                                </Typography>
+                            </Paper>
+
+                            <Typography variant="h4" color="white" align="center" sx={{ mt: 4 }} gutterBottom>
+                                Future of CoquiTones
+                            </Typography>
+
+                            <Paper sx={{ p: 2, backgroundColor: 'rgba(255, 255, 255, 0.1)', borderRadius: 10 }}>
+                                <Typography variant="body1" color="white">
                                     Given the educational nature of this endeavor and its non-commercial focus, we have decided to make our project Open-Source. This decision enables anyone to create their own adaptations based on their requirements.[MORE NEEDED]
                                 </Typography>
+                            </Paper>
 
-
-                            </Paper >
-                        </Grid>
-                    </Container >
-                </Box>
-            </Box>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
         </ThemeProvider>
     )
 }
--- Frontend/src/pages/Classifier.jsx
+++ Frontend/src/pages/Classifier.jsx
@@ -9,12 +9,85 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+
 import BarAndNav from "../components/shared/BarAndNav";
 import theme from "../components/shared/Theme"
 import FileUpload from "../components/shared/FileUpload";
 const Classifier = () => {
 
-    const [audioFile, setAudioFile] = useState(null)
+    const initDummyReport = () => {
+        const rows = [
+            { filename: 'coqui.WAV', crid: 1, tid: 2, crsamples: 100, crcoqui: 50, crantillensis: 30, crcochranae: 20, cre_monensis: 10, crgryllus: 5, crhedricki: 3, crlocustus: 2, crportoricensis: 1, crrichmondi: 0, crwightmanae: 0, crno_hit: 0 },
+
+        ];
+
+        return rows
+
+    }
+
+
+
+    const [report, setReport] = useState(initDummyReport())
+
+    const table = (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell aligh="center">File Name</TableCell>
+                        <TableCell align="center">ID</TableCell>
+                        <TableCell align="center">Samples</TableCell>
+                        <TableCell align="center">Coqui</TableCell>
+                        <TableCell align="center">Antillensis</TableCell>
+                        <TableCell>Cochranae</TableCell>
+                        <TableCell align="center">Monensis</TableCell>
+                        <TableCell align="center">Gryllus</TableCell>
+                        <TableCell align="center">Hedricki</TableCell>
+                        <TableCell align="center">locustus</TableCell>
+                        <TableCell>Portoricensis</TableCell>
+                        <TableCell align="center">Richmondi</TableCell>
+                        <TableCell align="center">Wighmanae</TableCell>
+                        <TableCell align="center">Total Detected</TableCell>
+
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {report.map((row) => (
+                        <TableRow
+                            key={row.filename}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.filename}
+                            </TableCell>
+                            <TableCell align="center">{row.crid}</TableCell>
+                            <TableCell align="center">{row.crsamples}</TableCell>
+                            <TableCell align="center">{row.crcoqui}</TableCell>
+                            <TableCell align="center">{row.crantillensis}</TableCell>
+                            <TableCell align="center">{row.crcochranae}</TableCell>
+                            <TableCell align="center">{row.cre_monensis}</TableCell>
+                            <TableCell align="center">{row.crgryllus}</TableCell>
+                            <TableCell align="center">{row.crhedricki}</TableCell>
+                            <TableCell align="center">{row.crlocustus}</TableCell>
+                            <TableCell align="center">{row.crportoricensis}</TableCell>
+                            <TableCell align="center">{row.crrichmondi}</TableCell>
+                            <TableCell align="center">{row.crwightmanae}</TableCell>
+                            <TableCell align="center">{row.crno_hit}</TableCell>
+
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+    const [rawAudioFile, setRawAudioFile] = useState(null)
     return (
         <ThemeProvider theme={theme}>
             <Box sx={{ display: 'flex' }} >
@@ -34,7 +107,7 @@ const Classifier = () => {
                 >
                     <Container maxWidth="lg" sx={{ mt: 10, mb: 10 }}>
                         <Grid item xs={12} md={8} lg={9}>
-                            <Paper
+                            <Paper elevation={4}
                                 sx={{
                                     p: 2,
                                     display: 'flex',
@@ -46,8 +119,26 @@ const Classifier = () => {
                                     Machine Learning Analysis
                                 </Typography>
 
-                                <FileUpload setAudioFile={setAudioFile} />
+                                <FileUpload setAudioFile={setRawAudioFile} />
                             </Paper >
+
+
+                            {
+                                rawAudioFile && <Paper elevation={4}
+                                    sx={{
+                                        p: 2,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        height: 'auto',
+                                    }}
+                                >
+                                    <Typography variant="h5" color="primary" align="center" gutterBottom>
+                                        Classifier Report
+                                    </Typography>
+
+                                    {table}
+                                </Paper>
+                            }
                         </Grid>
                     </Container >
                 </Box>
