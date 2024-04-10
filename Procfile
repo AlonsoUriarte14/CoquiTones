83fe56b27f10e91e36e0a5f7f8b5269d87602c9b
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-04-10 09:41:14 -0400

    trying without containers

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run build && uvicorn src.app:app --host 0.0.0.0 --port $PORT 
+web: sh setup.sh && uvicorn Backend.src.app:app --host 0.0.0.0 --port $PORT 
--- setup.sh
+++ setup.sh
@@ -2,11 +2,17 @@
 
 HEROKU_APP=coquitones
 
+# Add your setup commands here
+echo "Running setup script..."
+
+pip install --no-cache-dir --upgrade -r requirements.txt
+
 #Build frontend
 npm run build
 
-#init script for db
+# init script for db
 heroku pg:psql -a $HEROKU_APP < init.sql
 
+
 # run backend
-uvicorn src.app:app --host 0.0.0.0 --port $PORT 
+uvicorn Backend.src.app:app --host 0.0.0.0 --port $PORT 
