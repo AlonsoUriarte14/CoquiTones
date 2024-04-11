feded9f8bd5943a3f03a1076c13d35dd4c995a61
Edwin Camuy <edwin.camuy@upr.edu>
2024-04-11 13:06:19 -0400

    Wrote CSV preparation script

---
+++ ML/prepare_csv.py
@@ -0,0 +1,35 @@
+import json
+import os
+import sys
+import csv
+
+
+def import_suffix_map(path: str) -> dict[str, str]:
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def main() -> None:
+    target_folder = sys.argv[1]
+    suffix_map = import_suffix_map(
+        os.path.join(target_folder, "suffixmap.json"))
+    species_map = {v: k for k, v in suffix_map.items()}
+
+    files = []
+
+    for filename in os.listdir(target_folder):
+        name, ext = os.path.splitext(filename)
+        if ext == ".wav" or ext == ".WAV":
+            suffix = name[-3:]
+            species = species_map[suffix]
+            files.append({"filename": filename, "species": species})
+
+    with open(os.path.join(target_folder, 'samples.csv'), 'w', newline='') as output_file:
+        keys = files[0].keys()
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(files)
+
+
+if __name__ == "__main__":
+    main()
