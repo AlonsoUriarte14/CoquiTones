7a92c9ed861e8834752d7c30501ae11e81e3523b
AlonsoUriarte14 <diego.santiago6@upr.edu>
2024-04-16 14:28:53 -0400

    changes to prepare file for absolute patgh

--- ML/prepare_csv.py
+++ ML/prepare_csv.py
@@ -10,22 +10,33 @@ def import_suffix_map(path: str) -> dict[str, str]:
 
 
 def main() -> None:
-    target_folder = sys.argv[1]
-    suffix_map = import_suffix_map(
-        os.path.join(target_folder, "suffixmap.json"))
+    # Get the absolute path of the target folder
+    target_folder = os.path.abspath(sys.argv[1])
+
+    print(target_folder)
+    # Import suffix map
+    suffix_map = import_suffix_map(os.path.join(target_folder, "suffixmap.json"))
     species_map = {v: k for k, v in suffix_map.items()}
 
     files = []
 
     for filename in os.listdir(target_folder):
         name, ext = os.path.splitext(filename)
-        if ext == ".wav" or ext == ".WAV":
+        if ext.lower() == ".wav":
             suffix = name[-3:]
-            species = species_map[suffix]
-            files.append({"filename": filename, "species": species})
+            species = species_map.get(suffix)
+            if species:
+                files.append(
+                    {
+                        "filename": os.path.join(target_folder, filename),
+                        "species": species,
+                    }
+                )
 
-    with open(os.path.join(target_folder, 'samples.csv'), 'w', newline='') as output_file:
-        keys = files[0].keys()
+    with open(
+        os.path.join(target_folder, "samples.csv"), "w", newline=""
+    ) as output_file:
+        keys = files[0].keys() if files else ["filename", "species"]
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(files)
