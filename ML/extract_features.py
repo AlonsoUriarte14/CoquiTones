516359b1fcbc9589dd11b1ea493d765a8daba804
Edwin Camuy <edwin.camuy@upr.edu>
2024-04-24 20:28:47 -0400

    Cleaned up feature extraction recipe

--- ML/extract_features.py
+++ ML/extract_features.py
@@ -3,12 +3,15 @@ import dataiku
 import pandas as pd
 import numpy as np
 from dataiku import pandasutils as pdu
+from dataiku import recipe
 import librosa
-import threading
+from concurrent.futures import ThreadPoolExecutor
+
+input_dataset = recipe.get_input(object_type='DATASET')
+output_dataset = "Train_prepared"
 
 # Read recipe inputs
-samples = dataiku.Dataset("Train")
-samples_df = samples.get_dataframe()
+samples_df = input_dataset.get_dataframe()
 
 
 # Define the extract_features function
@@ -41,48 +44,19 @@ def extract_features(file_path):
     return result
 
 
-def apply_extract_features(file_paths, results, index):
-    """
-    Apply the extract_features function to each audio file in a subset of file paths.
+def main():
+    print(samples_df.head())
+    print(f"Processing {len(samples_df)} files.")
+    with ThreadPoolExecutor() as pool:
+        results = pool.map(
+            extract_features, samples_df["filename"])
 
-    Args:
-        file_paths (list): List of file paths.
-        results (list): List to store the results.
-        index (int): Index of the worker thread.
-    """
-    print(f"Worker {index + 1} started.")
-    for file_path in file_paths:
-        results.append("[" + ",".join(map(str, extract_features(file_path))) + "]")
-    print(f"Worker {index + 1} finished.")
+    # Add the results to the DataFrame
+    samples_df["spectrogram"] = pd.Series(results)
+
+    # Write recipe outputs
+    output_dataset.write_with_schema(samples_df)
 
 
-# Split the file paths into subsets
-num_threads = 6
-file_paths = samples_df["filename"].tolist()
-chunk_size = (len(file_paths) + num_threads - 1) // num_threads
-file_path_chunks = [
-    file_paths[i : i + chunk_size] for i in range(0, len(file_paths), chunk_size)
-]
-
-# Initialize a list to store the results
-results = []
-
-# Create and start worker threads
-threads = []
-for i, file_path_chunk in enumerate(file_path_chunks):
-    thread = threading.Thread(
-        target=apply_extract_features, args=(file_path_chunk, results, i)
-    )
-    threads.append(thread)
-    thread.start()
-
-# Wait for all threads to complete
-for thread in threads:
-    thread.join()
-
-# Add the results to the DataFrame
-samples_df["spectrogram"] = pd.Series(results)
-
-# Write recipe outputs
-SpecData = dataiku.Dataset("Train_prepared")
-SpecData.write_with_schema(samples_df)
+if __name__ == '__main__':
+    main()
